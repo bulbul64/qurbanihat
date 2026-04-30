7cd94be2e8597996111407f7abc4a2a1f87fec81
bulbul64 <bbb46483@gmail.com>
2026-04-30 23:57:32 +0600

    fix: resolve env issue causing startsWith error in auth

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,6 +1,7 @@
 import dns from "node:dns";
 dns.setServers(["8.8.8.8", "8.8.4.4"]);
 
+
 import { betterAuth } from "better-auth";
 import { MongoClient } from "mongodb";
 import { mongodbAdapter } from "better-auth/adapters/mongodb";
@@ -9,18 +10,16 @@ const url = process.env.MONGODB_URL!;
 
 const client = new MongoClient(url);
 
-const db = client.db("qurbanihat"); // safe DB name explicitly
+const db = client.db('qurbanihat');
 
-if (!db) throw new Error("Database not found");
-
-const baseURL = process.env.BETTER_AUTH_URL || "http://localhost:3000";
+if(!db) throw new Error("Database not found");
 
 export const auth = betterAuth({
-  baseURL,
-  emailAndPassword: {
-    enabled: true,
-  },
+  emailAndPassword: { 
+    enabled: true, 
+  }, 
   database: mongodbAdapter(db, {
-    client,
+    // Optional: if you don't provide a client, database transactions won't be enabled.
+    client
   }),
 });
